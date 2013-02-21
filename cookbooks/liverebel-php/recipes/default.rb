0811657703220a9d38cb10a82244a26123a0c146
Geert Bevin <gbevin@uwyn.com>
2013-02-21 19:06:40 +0200

    Enabled mod_rewrite and php5-mysql on PHP nodes

--- cookbooks/liverebel-php/recipes/default.rb
+++ cookbooks/liverebel-php/recipes/default.rb
@@ -4,7 +4,12 @@ include_recipe "php"
 include_recipe "phpunit"
 include_recipe "apache2"
 include_recipe "apache2::mod_php5"
+include_recipe "apache2::mod_rewrite"
 include_recipe "liverebel-file-agent"
+
+package "php5-mysql" do
+  action :install
+end
 
 execute "enable-default-site" do
   command "sudo a2ensite default"
