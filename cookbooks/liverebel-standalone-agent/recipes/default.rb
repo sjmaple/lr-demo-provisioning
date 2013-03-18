df902177e6cd0886b05c667b4302047397599f45
Geert Bevin <gbevin@uwyn.com>
2013-03-18 18:35:29 +0100

    Made agent cookbooks possible to exist together

--- cookbooks/liverebel-standalone-agent/recipes/default.rb
+++ cookbooks/liverebel-standalone-agent/recipes/default.rb
@@ -1,50 +1,50 @@
-agent_user = node['liverebel']['agent']['user']
-agent_group = node['liverebel']['agent']['group']
-agent_type = node['liverebel']['agent']['type']
-agent_user_home = "/opt/#{agent_user}"
-agent_installer_jar = "lr-#{agent_type}-agent-installer.jar"
-agent_installer_jar_path = "#{agent_user_home}/#{agent_installer_jar}"
-agent_installed_path = "#{agent_user_home}/lr-agent"
+standalone_agent_user = node['liverebel']['agent']['user']
+standalone_agent_group = node['liverebel']['agent']['group']
+standalone_agent_type = node['liverebel']['agent']['type']
+standalone_agent_user_home = "/opt/#{standalone_agent_user}"
+standalone_agent_installer_jar = "lr-#{standalone_agent_type}-agent-installer.jar"
+standalone_agent_installer_jar_path = "#{standalone_agent_user_home}/#{standalone_agent_installer_jar}"
+standalone_agent_installed_path = "#{standalone_agent_user_home}/lr-agent"
 
-user "create_agent_user" do
+user "create_standalone_agent_user" do
   comment "LiveRebel agent user"
-  username agent_user
-  home "#{agent_user_home}"
+  username standalone_agent_user
+  home "#{standalone_agent_user_home}"
   manage_home true
-  if agent_group
-    gid agent_group
+  if standalone_agent_group
+    gid standalone_agent_group
   end
   shell "/bin/bash"
   action :create
 end
 
-ruby_block 'update-agent-properties' do
+ruby_block 'update-standalone-agent-properties' do
   action :nothing
   block do
-    file = Chef::Util::FileEdit.new("#{agent_installed_path}/conf/lr-agent.properties")
+    file = Chef::Util::FileEdit.new("#{standalone_agent_installed_path}/conf/lr-agent.properties")
     file.insert_line_if_no_match("/agent.host/", "agent.host=#{node['liverebel']['agentip']}")
     file.write_file
   end
 end
 
-execute "install-agent" do
-  cwd agent_user_home
-  user agent_user
-  command "/usr/bin/java -Dliverebel.host=#{node['liverebel']['hostip']} -jar #{agent_installer_jar_path}"
+execute "install-standalone-agent" do
+  cwd standalone_agent_user_home
+  user standalone_agent_user
+  command "/usr/bin/java -Dliverebel.host=#{node['liverebel']['hostip']} -jar #{standalone_agent_installer_jar_path}"
   action :nothing
-  notifies :create, "ruby_block[update-agent-properties]", :immediately
+  notifies :create, "ruby_block[update-standalone-agent-properties]", :immediately
   not_if do
-    File.exists?(agent_installed_path)
+    File.exists?(standalone_agent_installed_path)
   end
 end
 
-remote_file agent_installer_jar_path do
-  source "https://#{node['liverebel']['hostip']}:9001/public/#{agent_installer_jar}"
-  owner agent_user
+remote_file standalone_agent_installer_jar_path do
+  source "https://#{node['liverebel']['hostip']}:9001/public/#{standalone_agent_installer_jar}"
+  owner standalone_agent_user
   mode 00644
-  notifies :run, "execute[install-agent]", :immediately
+  notifies :run, "execute[install-standalone-agent]", :immediately
   not_if do
-    File.exists?(agent_installer_jar_path)
+    File.exists?(standalone_agent_installer_jar_path)
   end
 end
 
@@ -56,8 +56,8 @@ when "debian","ubuntu"
     group "root"
     mode "0755"
     variables(
-      :lragent_user => agent_user,
-      :lragent_home => agent_installed_path
+      :lragent_user => standalone_agent_user,
+      :lragent_home => standalone_agent_installed_path
     )
   end
   execute "init-deb" do
