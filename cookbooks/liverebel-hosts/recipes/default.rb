e531a1d16e1d914118ece92b5c8a7b749d980c0f
Geert Bevin <gbevin@uwyn.com>
2013-03-20 09:01:23 +0100

    Fixes to host entry handling

--- cookbooks/liverebel-hosts/recipes/default.rb
+++ cookbooks/liverebel-hosts/recipes/default.rb
@@ -1,8 +1,31 @@
+# start from a clean slate every time
+hostsfile_entry "#{node["hosts"]["host"]}" do
+  action    :remove
+end
+hostsfile_entry "#{node["hosts"]["java"]}" do
+  action    :remove
+end
+hostsfile_entry "#{node["hosts"]["java1"]}" do
+  action    :remove
+end
+hostsfile_entry "#{node["hosts"]["java2"]}" do
+  action    :remove
+end
+hostsfile_entry "#{node["hosts"]["php"]}" do
+  action    :remove
+end
+hostsfile_entry "#{node["hosts"]["php1"]}" do
+  action    :remove
+end
+hostsfile_entry "#{node["hosts"]["php2"]}" do
+  action    :remove
+end
+
+# add latest versions for the hosts
 hostsfile_entry "#{node["hosts"]["host"]}" do
   hostname  'host.answers.liverebel.com'
   action    :append
 end
-
 hostsfile_entry "#{node["hosts"]["java"]}" do
   hostname  'java.answers.liverebel.com'
   action    :append
